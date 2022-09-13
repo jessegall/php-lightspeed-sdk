e4e222e402f703bc41ea26d5a23b69ed612c1cd9
Jesse Gall <j.gall@brisp.nl>
2022-09-13 23:37:58 +0200

    Updated README.md

--- tests/ResourceTest.php
+++ tests/ResourceTest.php
@@ -3,31 +3,39 @@
 namespace JesseGall\LightspeedSDK\Tests;
 
 use JesseGall\LightspeedSDK\Resources\Order;
+use JesseGall\LightspeedSDK\Resources\Variant;
 
 class ResourceTest extends TestCase
 {
 
     public function test_resource()
     {
-        $one = (new Order)->setId(224905911)
+        $one = (new Order)
+            ->setId(224905911)
             ->hydrate()
             ->getProducts()
             ->first()
             ->getVariant();
 
-        $two = (new Order)->setId(224854308)
+        $two = (new Order)
+            ->setId(224854308)
             ->hydrate()
             ->getProducts()
             ->first()
             ->getVariant();
 
-        $three = (new Order)->setId(224854308)
+        $three = (new Order)
+            ->setId(224854308)
             ->hydrate()
             ->getProducts()
             ->first()
             ->getVariant();
 
-        dd($one->getId(), $two->getId(), $three->getId());
+        $variant = (new Variant)
+            ->setId(243575637)
+            ->hydrate();
+
+        dd($one->getId(), $two->getId(), $three->getId(), $variant);
     }
 
 
